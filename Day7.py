59e08eaffe65601ef38037da46bc33461484a270
Divya <sabbidivyareddy25@gmail.com>
2026-01-11 15:08:12 +0530

    Day 7 practice

---
+++ Day7.py
@@ -0,0 +1,25 @@
+# %%
+#If condition practice to check whether the given number is positive.
+num = int(input("Enter a number: "))
+is_positive = num > 0
+if is_positive:
+    print("Number is positive")
+# %%
+#Checking whether the student is passed in Physics
+physics = 78
+is_pass = physics >=40
+if is_pass:
+    print("Passed in Physics")
+# %%
+#Checking the largest number
+a = 30
+b = 20
+if a > b:
+     print("a is positive")
+if b > a:
+    print("b is positive")
+
+
+
+
+
